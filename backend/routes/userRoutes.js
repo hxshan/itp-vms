096e99c88e9d03f864a43a10f4bc8a31f8ddca6a
Heshan Gimhana <galgodageheshan@gmail.com>
2024-03-07 12:57:01 +0530

    login page-partial finish

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,6 @@
+const express = require('express')  
+const Auth = require('../middleware/Auth')
+
+const router = express.Router()
+
+router.post('/login',()=>{}) 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AdminDashboard from "./pages/admin/AdminDashboard";
 import HireDashboard from "./pages/hires/HireDashboard";
+import {Login} from "./pages/shared"
 function App() {
   const queryClient = new QueryClient();
 
@@ -10,6 +11,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <main>
         <Routes>        
+          <Route path="/login" element={<Login/>}/>
           <Route path="/admin" element={<AdminDashboard/>}/>
           <Route path="/hires" element={<HireDashboard/>}/>
         </Routes>
