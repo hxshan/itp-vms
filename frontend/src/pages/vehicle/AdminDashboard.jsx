c06897cd9df237d97dde394d03a1f54d2817c972
Malith Gihan <125493545+MalithGihan@users.noreply.github.com>
2024-03-09 22:14:11 +0530

    create folder

---
+++ frontend/src/pages/vehicle/AdminDashboard.jsx
@@ -0,0 +1,29 @@
+import useAxiosGet from "@/hooks/useAxiosGet"
+import {EmployeeTable,CreateUserForm} from "../../components/admin/"
+import { useEffect, useState } from "react"
+import { Navbar } from "../shared"
+
+const AdminDashboard = () => {
+
+    const columns =["Name ","Email","IDK foreal","something","Actions"]
+    const [users,setUsers]=useState([])
+    const {data:userData,error:userErr,isLoading:userIsLoading,refetch:userRefetch} = useAxiosGet('/user/getall')
+
+    useEffect(()=>{
+      if(userData !=null)
+        setUsers(userData)
+    },[userData])
+
+//<EmployeeTable columns={columns} data={data}/>
+    console.log(userData)
+  return (
+  
+    <div className="container mx-auto py-10">
+        <CreateUserForm />
+        <EmployeeTable columns={columns} data={users} isLoading={userIsLoading}/>
+     </div>
+
+  )
+}
+
+export default AdminDashboard
