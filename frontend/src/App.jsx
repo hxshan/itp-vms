cb656d85961870e6c934c7f194b125c24494776c
Malith Gihan <125493545+MalithGihan@users.noreply.github.com>
2024-03-10 21:08:23 +0530

    make route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AdminDashboard from "./pages/admin/AdminDashboard";
 import HireDashboard from "./pages/hires/HireDashboard";
 import {Login,Navbar} from "./pages/shared"
+import AddVehicle from "./pages/vehicle/AddVehicle"
+
 function App() {
   const queryClient = new QueryClient();
 
@@ -15,6 +17,7 @@ function App() {
           <Route path="/login" element={<Login/>}/>
           <Route path="/admin" element={<AdminDashboard/>}/>
           <Route path="/hires" element={<HireDashboard/>}/>
+          <Route path="/add_vehicle" element={<AddVehicle/>}/>
         </Routes>
       </main>
     </QueryClientProvider>
