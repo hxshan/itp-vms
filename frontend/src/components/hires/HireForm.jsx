6cb5df82628c8ea82d5a96b6ef3d6db325d92f25
Chamod Perera <chamod914@gmail.com>
2024-03-13 21:32:39 +0530

    Update Hire Form

--- frontend/src/components/hires/HireForm.jsx
+++ frontend/src/components/hires/HireForm.jsx
@@ -29,26 +29,34 @@ const Form = ({setShowForm }) => {
   const [airCondition, setAirCondition] = useState(false);
   const [vehicle, setVehicle] = useState('')
   const [driver, setDriver] = useState('')
+  const [startPoint, setStartPoint] = useState('')
+  const [endPoint, setEndPoint] = useState('')
+  const [tripType, setTripType] = useState(false)
+  const [distence, setSetDistence] = useState()
+  const [cusName, setCusName] = useState('')
+  const [cusEmail, setCusEmail] = useState('')
+  const [cusMobile, setCusMobile] = useState('')
+  const [cusNic, setCusNic] = useState('')
 
 
 
   return (
-    <div className="w-full h-full flex bg-gray-200 px-[20px] py-[20px] justify-center items-center">
+    <div className="w-full h-full flex bg-gray-200 px-2 py-[20px] justify-center align-center xl:px-[60px] xl:py-[50px]">
 
-      <div className="w-full h-full bg-white px-3">
+      <div className="w-full h-full bg-white px-3 py-5 xl:px-10">
         {/*Titile*/}
         <div className="text-center pt-[10px] pb-8 border-b-2 border-[#37A000] ">
-          <h1 className="text-2xl font-semibold">Add Hire</h1>
+          <h1 className="text-2xl font-semibold xl:text-4xl">Add Hire</h1>
         </div>
 
         {/* Form */}
         {step === 1 && (
-          <div className="mt-10  w-full border-2 border-black pt-5 px-4">
+          <div className="mt-10  w-full border-2 border-black pt-5 px-4 xl:px-12 xl:py-10">
 
             {/* Date Section */}
-            <div className='flex flex-col justify-between align-baseline mt-3'> 
+            <div className='flex flex-col justify-between align-baseline mt-3 xl:flex-row'> 
 
-              <div className="mb-8 flex justify-between items-baseline">
+              <div className="mb-10 flex justify-between align-baseline xl:flex-1 xl:mr-14">
                     <label htmlFor="startDate" 
                     className="block font-medium text-black mr-[10px] text-base">
                       Start Date
@@ -61,7 +69,7 @@ const Form = ({setShowForm }) => {
                     />
                 </div>
 
-                <div className="mb-8 flex justify-between items-baseline">
+                <div className="mb-10 flex justify-between align-baseline flex-1">
 
                     <label htmlFor="endDate" 
                     className="block font-medium text-black mr-[10px] text-base">
@@ -78,41 +86,45 @@ const Form = ({setShowForm }) => {
             </div>
 
             {/* Vehicle Section */}
-            <div> 
+            <div className='flex flex-col justify-between align-baseline '> 
 
-              <div className="mb-8 flex justify-between items-baseline ">
+              <div className='flex flex-col justify-between align-baseline xl:flex-row'>
+                <div className="mb-10 flex justify-between align-baseline xl:flex-1 xl:mr-14">
 
-                <label htmlFor="vehicleType" 
-                className="block font-medium text-black mr-[10px] text-base">
-                  Vehicle Type
-                </label>
+                    <label htmlFor="vehicleType" 
+                    className="block font-medium text-black mr-[10px] text-base">
+                      Vehicle Type
+                    </label>
 
-                <select id="vehicleType" name="vehicleType" 
-                value={vehicleType} onChange={(e) => setVehicleType(e.target.value)} 
-                className='border-2 rounded border-black px-5 '
-                >
-                    <option value={"Car"}>Car</option>
-                    <option value={"van"}>Van</option>
-                    <option value={"Bus"}>Bus</option>
-                    <option value={"Lorry"}>Lorry</option>
-                </select>
+                    <select id="vehicleType" name="vehicleType" 
+                    value={vehicleType} onChange={(e) => setVehicleType(e.target.value)} 
+                    className='border-2 rounded border-black px-14'
+                    >
+                        <option value={"Car"}>Car</option>
+                        <option value={"van"}>Van</option>
+                        <option value={"Bus"}>Bus</option>
+                        <option value={"Lorry"}>Lorry</option>
+                    </select>
+
+                    </div>
+
+                    <div className="mb-10 flex justify-between align-baseline xl:flex-1">
+                    <label htmlFor="vehicleSubcategory" 
+                    className="block font-medium text-black mr-[10px] text-base">
+                      Vehicle Subcategory
+                    </label>
+
+                    <select id="vehicleSubcategory" name="vehicleSubcategory" 
+                    value={vehicleSubcategory} onChange={(e) => setVehicleSubcategory(e.target.value)} 
+                    className='border-2 rounded border-black px-14'>
+                        {/* Options for vehicle subcategories */}
+                    </select>
+                    </div>
 
               </div>
+              
 
-              <div className="mb-8 flex justify-between items-baseline">
-                <label htmlFor="vehicleSubcategory" 
-                className="block font-medium text-black mr-[10px] text-base">
-                  Vehicle Subcategory
-                </label>
-                
-                <select id="vehicleSubcategory" name="vehicleSubcategory" 
-                value={vehicleSubcategory} onChange={(e) => setVehicleSubcategory(e.target.value)} 
-                className='border-2 rounded border-black px-5'>
-                    {/* Options for vehicle subcategories */}
-                </select>
-              </div>
-
-              <div className="mb-8 flex justify-start items-baseline">
+              <div className="mb-10 flex justify-start align-baseline">
 
                     <label htmlFor="airCondition" 
                     className="block font-medium text-black mr-[10px] text-base mr-8">
@@ -131,10 +143,10 @@ const Form = ({setShowForm }) => {
             {/* Passenger Count */}
             <div>
 
-              <div className="mb-8 flex justify-between items-baseline ">
+              <div className="mb-10 flex justify-between align-baseline xl:justify-start">
 
                 <label htmlFor="passengerCount" 
-                className="block font-medium text-black mr-[5px] text-base">
+                className="block font-medium text-black mr-[5px] text-base xl:mr-7">
                   No of Passengers
                 </label>
 
@@ -146,43 +158,213 @@ const Form = ({setShowForm }) => {
 
               </div> 
 
-            </div>
-
-                
-
-                
+            </div>     
 
           </div>
         )}
 
+          {/*Section 2 */}
         {step === 2 && (
-          <div className="mt-8 px-4">
-            <div>
-              <label htmlFor='Select Vehicle' className="ml-2 block text-sm font-medium text-gray-700">Select Vehical</label>
-              <select id="avalableVehicle" name="avalableVehicle" value={vehicle} onChange={(e) => setVehicle(e.target.value)} >
-                        <option value={"Car"}>Car</option>
-                        <option value={"van"}>Van</option>
-                        <option value={"Bus"}>Bus</option>
-                        <option value={"Lorry"}>Lorry</option>
-                    </select>
+          
+          <div className="mt-5 px-2">
+            {/* Vehicle and driver section */}
+            <div className='flex justify-between align-baseline mb-5 '>
+                <div className=" flex flex-col justify-between align-baseline xl:flex-1 xl:mr-14 xl:flex-row">
+
+                  <label htmlFor="AssignVehicle" 
+                    className="block font-medium text-black mr-[10px] text-base">
+                      Select Vehicle
+                  </label>
+
+                  <select id="vehicle" name="vehicle" 
+                    value={vehicle} onChange={(e) => setVehicle(e.target.value)} 
+                    className='border-2 rounded border-black px-14'
+                    >
+                      <option value={"CHJ-2233"}>CHJ-2233</option>
+                      <option value={"CHJ-2233"}>CHJ-2233</option>
+
+                  </select>
+
+                </div>
+
+                <div className=" flex flex-col justify-between align-baseline xl:flex-1 xl:flex-row">
+                  <label htmlFor="assignDriver" 
+                    className="block font-medium text-black mr-[10px] text-base">
+                      Select Driver
+                  </label>
+
+                  <select id="driverList" name="driverList" 
+                    value={driver} onChange={(e) => setDriver(e.target.value)} 
+                    className='border-2 rounded border-black px-14'>
+                      <option value={"Chamara"}>Chamara</option>
+                      <option value={"Chanchala"}>Chanchala</option>
+                      <option value={"Danny"}>Danny</option>
+                      <option value={"Jonny"}>Jonny</option>
+                  </select>
+                </div>
+
             </div>
 
-            <div>
-              <label htmlFor='Select Driver' className="ml-2 block text-sm font-medium text-gray-700">Select Driver</label>
-              <select id="driver" name="driver" value={vehicle} onChange={(e) => setDriver(e.target.value)} >
-                        <option value={"Car"}>Car</option>
-                        <option value={"van"}>Van</option>
-                        <option value={"Bus"}>Bus</option>
-                        <option value={"Lorry"}>Lorry</option>
-              </select>
+            {/* Trip details */}
+            <div className=' border-b-2 border-black xl:flex xl:flex-col xl:justify-between xl:align-baseline'>
+
+              <div className='flex justify-between align-baseline mb-5'>
+                <div className=" flex flex-col justify-between align-baseline mr-2 xl:flex-row xl:flex-1 xl:mr-14">
+
+                  <label htmlFor="startPoint" 
+                  className="block font-medium text-black  text-base xl:mr-7">
+                    Starting Point
+                  </label>
+
+                  <input type="text" id="startPoint" name="startPoint" 
+                  value={startPoint}
+                  onChange={(e) => setStartPoint(e.target.value)} 
+                  placeholder='From'
+                  className='border-2 rounded border-black px-4'
+                  />
+
+                </div> 
+
+                <div className=" flex flex-col justify-between align-baseline xl:flex-row xl:flex-1">
+
+                  <label htmlFor="endPoint" 
+                  className="block font-medium text-black mr-[5px] text-base xl:mr-7">
+                    End Point
+                  </label>
+
+                  <input type="text" id="endPoint" name="endPoint" 
+                  value={endPoint}
+                  onChange={(e) => setEndPoint(e.target.value)} 
+                  placeholder='To'
+                  className='border-2 rounded border-black px-4'
+                  />
+
+                </div>
+                
+              </div>
+
+              <div  className='flex justify-between align-baseline xl:justify-start'>  
+
+                <div className="mb-6 flex justify-start align-baseline xl:mr-16">
+
+                  <label htmlFor="tripType" 
+                  className="block font-medium text-black mr-[10px] text-base mr-8">
+                    Round Trip
+                  </label>
+
+                  <input type="checkbox" id="roundtrip" name="roundtrip" 
+                  checked={tripType} onChange={(e) => setTripType(e.target.checked)} 
+                  className=''/>
+
+                  </div>
+
+                  <div className="mb-6 flex justify-between align-baseline xl:justify-start">
+
+                  <label htmlFor="distence" 
+                  className="block font-medium text-black mr-[5px] text-base xl:mr-7">
+                    Distence
+                  </label>
+
+                  <input type="text" id="distence" name="distence" 
+                  value={distence}
+                  onChange={(e) => setSetDistence(e.target.value)} 
+                  placeholder='Estimate Distence'
+                  className='border-2 rounded border-black px-4'
+                  />
+
+                </div>
+
+              </div>
+              
+
+              
+
             </div>
+
+            {/* Customer Details */}
+            <div className='mt-7'> 
+              <div className=''> 
+                <div className="mb-7 flex justify-start align-baseline xl:justify-start">
+
+                  <label htmlFor="cusName" 
+                  className="block mr-7 font-medium text-black text-base ">
+                    Name
+                  </label>
+
+                  <input type="text" id="cusName" name="cusName" 
+                  value={cusName}
+                  onChange={(e) => setCusName(e.target.value)} 
+                  placeholder='Customer Name'
+                  className='border-2 rounded border-black w-[100%] px-2 xl:px-4'
+                  />
+
+                </div>
+
+                <div className="mb-7 flex jjustify-start align-baseline xl:justify-start">
+
+                  <label htmlFor="cusEmail" 
+                  className="block font-medium text-black mr-7 text-base xl:mr-7">
+                    Email
+                  </label>
+
+                  <input type="text" id="cusEmail" name="cusEmail" 
+                  value={cusEmail}
+                  onChange={(e) => setCusEmail(e.target.value)} 
+                  placeholder='Customer Email'
+                  className='border-2 rounded border-black w-[100%] px-2 xl:px-4'
+                  />
+
+                </div>
+
+              </div>
+
+              <div className='flex justify-between'>
+
+                <div className="mb-5  flex flex-col justify-between align-baseline xl:flex-row xl:flex-1 xl:mr-14">
+
+                  <label htmlFor="cusMobile" 
+                  className="block font-medium text-black mr-[5px] text-base xl:mr-7">
+                    Mobile
+                  </label>
+
+                  <input type="text" id="cusMobile" name="cusMobile" 
+                  value={cusMobile}
+                  onChange={(e) => setCusMobile(e.target.value)} 
+                  placeholder='Customer Mobile No'
+                  className='border-2 px-2 rounded border-black xl:px-4'
+                  />
+
+                </div>
+
+                <div className="mb-5  flex flex-col justify-between align-baseline xl:flex-row xl:flex-1">
+
+                  <label htmlFor="cusNic" 
+                  className="block font-medium text-black mr-[5px] text-base xl:mr-7">
+                    Nic
+                  </label>
+
+                  <input type="text" id="cusNic" name="cusNic" 
+                  value={cusNic}
+                  onChange={(e) => setCusNic(e.target.value)} 
+                  placeholder='Customer NIC'
+                  className='border-2 px-2 rounded border-black xl:px-4
+                  '
+                  />
+
+                </div> 
+                
+              </div>
+            </div>
+            
+
 
           </div>
         )}
 
+          {/* Confirmation */}
         {step === 3 && (
-          <div className="mt-8 px-4">
-            <h2 className="text-lg font-semibold mb-4">Step 3: Experience</h2>
+          <div className="mt-3 px-4">
+            <h2 className="text-2xl font-semibold text-center mb-4 underline ">Confirmation</h2>
             {/* Form fields for step 3 */}
           </div>
         )}
