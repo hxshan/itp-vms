036de4c9add29edaf6f74b956891e9d36961f285
Chamod Perera <chamod914@gmail.com>
2024-03-19 19:45:18 +0530

    Update Hire Table

--- frontend/src/components/hires/HireTable.jsx
+++ frontend/src/components/hires/HireTable.jsx
@@ -1,13 +1,56 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ViewHire from "./ViewHire";
+import {hire1, hire2, hire3} from "./testdata"
+import PropTypes from 'prop-types'
 
-const HireTable = () => {
+const HireTable = ({vehicleNo , setResults, results}) => {
+
+    HireTable.propTypes = {
+        setResults: PropTypes.func.isRequired,
+        vehicleNo: PropTypes.string.isRequired,
+        results: PropTypes.array.isRequired
+      };
+
+    const [hireData, setHireData] = useState([{
+        id : 0,
+        startDate: new Date(),
+        endDate: new Date(),
+        vehicleType: "",
+        vehicleSubcategory: "",
+        passengerCount: 0,
+        airCondition: false,
+        vehicle: "",
+        driver: "",
+        startPoint: "",
+        endPoint: "",
+        tripType: false,
+        distence: 0,
+        cusName: "",
+        cusEmail: "",
+        cusMobile: "",
+        cusNic: ""
+    }])
+
+    useEffect(() => {
+        setHireData([hire1, hire2, hire3]);
+    }, [])
 
     const [viewHire, setViewHire] = useState(false)
+    const [viewHireData, setViewHireData] = useState(null)
 
-    const handleView = () => {
+    const handleView = (hId) => {
+        const selected = hireData.find((hire) => hire.id === hId);
+        setViewHireData(selected)
         setViewHire(!viewHire)
     }
+
+    //Search function
+
+    useEffect(() => {
+        const result = hireData.filter((hire) => 
+        hire.vehicle.toLowerCase().includes(vehicleNo.toLowerCase()))
+        setResults(result)
+    }, [hireData, vehicleNo])
 
   return (
     <div className="w-full h-full flex px-2 py-[20px] justify-center align-center xl:px-[60px] xl:py-[50px] ">
@@ -15,6 +58,7 @@ const HireTable = () => {
                     <thead className="border-b-2 border-black">
                         <tr>
                             <th className="px-4 py-2">Hire ID</th>
+                            <th className="px-4 py-2">Vehicle No</th>
                             <th className="px-4 py-2">Start Date</th>
                             <th className="px-4 py-2">End Date</th>
                             <th className="px-4 py-2">Status</th>
@@ -22,74 +66,30 @@ const HireTable = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr className="border-b-2 border-black">
-                            <td className="px-4 py-2">1</td>
-                            <td className="px-4 py-2">15/02/2024</td>
-                            <td className="px-4 py-2">17/02/2024</td>
-                            <td className="px-4 py-2">Active</td>
-                            <td className="px-4 py-4 flex justify-between items-baseline">
-                                <button 
-                                className="px-2 py-1 bg-[#D4D800] text-white rounded-md mr-2"
-                                onClick={handleView}>
-                                    View
-                                </button>
-                                <button className="px-2 py-1 bg-[#0E6300] text-white rounded-md mr-2">Edit</button>
-                                <button className="px-2 py-1 bg-[#A90000] text-white rounded-md">Delete</button>
-                            </td>
-                        </tr>
+                        {(vehicleNo ? results : hireData).map((hire) => 
+                            <tr key={hire.id} className="border-b-2 border-black">
+                                <td className="px-4 py-2">{hire.id}</td>
+                                <td className="px-4 py-2">{hire.vehicle}</td>
+                                <td className="px-4 py-2">{new Date(hire.startDate).toLocaleDateString()}</td>
+                                <td className="px-4 py-2">{new Date(hire.endDate).toLocaleDateString()}</td>
+                                <td className="px-4 py-2">Active</td>
+                                <td className="px-4 py-4 flex justify-between items-baseline">
+                                    <button 
+                                    className="px-2 py-1 bg-[#D4D800] text-white rounded-md mr-2"
+                                    onClick={() => handleView(hire.id)}>
+                                        View
+                                    </button>
+                                    <button className="hidden xl:grid px-2 py-1 bg-[#0E6300] text-white rounded-md mr-2">Edit</button>
+                                    <button className="hidden xl:grid px-2 py-1 bg-[#A90000] text-white rounded-md">Delete</button>
+                                </td>
+                            </tr>
+                        )}
 
-                        <tr className="border-b-2 border-black">
-                            <td className="px-4 py-2">1</td>
-                            <td className="px-4 py-2">15/02/24</td>
-                            <td className="px-4 py-2">17/02/24</td>
-                            <td className="px-4 py-2">Active</td>
-                            <td className="px-4 py-4 flex justify-between items-baseline">
-                                <button className="px-2 py-1 bg-[#D4D800] text-white rounded-md mr-2">View</button>
-                                <button className="px-2 py-1 bg-[#0E6300] text-white rounded-md mr-2">Edit</button>
-                                <button className="px-2 py-1 bg-[#A90000] text-white rounded-md">Delete</button>
-                            </td>
-                        </tr>
-
-                        <tr className="border-b-2 border-black">
-                            <td className="px-4 py-2">1</td>
-                            <td className="px-4 py-2">15/02/24</td>
-                            <td className="px-4 py-2">17/02/24</td>
-                            <td className="px-4 py-2">Active</td>
-                            <td className="px-4 py-4 flex justify-between items-baseline">
-                                <button className="px-2 py-1 bg-[#D4D800] text-white rounded-md mr-2">View</button>
-                                <button className="px-2 py-1 bg-[#0E6300] text-white rounded-md mr-2">Edit</button>
-                                <button className="px-2 py-1 bg-[#A90000] text-white rounded-md">Delete</button>
-                            </td>
-                        </tr>
-
-                        <tr className="border-b-2 border-black">
-                            <td className="px-4 py-2">1</td>
-                            <td className="px-4 py-2">15/02/24</td>
-                            <td className="px-4 py-2">17/02/24</td>
-                            <td className="px-4 py-2">Active</td>
-                            <td className="px-4 py-4 flex justify-between items-baseline">
-                                <button className="px-2 py-1 bg-[#D4D800] text-white rounded-md mr-2">View</button>
-                                <button className="px-2 py-1 bg-[#0E6300] text-white rounded-md mr-2">Edit</button>
-                                <button className="px-2 py-1 bg-[#A90000] text-white rounded-md">Delete</button>
-                            </td>
-                        </tr>
-
-                        <tr className="border-b-2 border-black">
-                            <td className="px-4 py-2">1</td>
-                            <td className="px-4 py-2">15/02/24</td>
-                            <td className="px-4 py-2">17/02/24</td>
-                            <td className="px-4 py-2">Active</td>
-                            <td className="px-4 py-4 flex justify-between items-baseline">
-                                <button className="px-2 py-1 bg-[#D4D800] text-white rounded-md mr-2">View</button>
-                                <button className="px-2 py-1 bg-[#0E6300] text-white rounded-md mr-2">Edit</button>
-                                <button className="px-2 py-1 bg-[#A90000] text-white rounded-md">Delete</button>
-                            </td>
-                        </tr>
                     </tbody>
                 </table>
 
 
-                {viewHire && <ViewHire setViewHire = {setViewHire} />}
+                {viewHire && <ViewHire setViewHire = {setViewHire} viewHireData = {viewHireData}/>}
                 
     </div>
   );
