ca409cfeabec9a62e3581d1e3a5db7155f47c331
Chamod Perera <chamod914@gmail.com>
2024-03-19 19:49:45 +0530

    Update

--- frontend/src/components/hires/ViewHire.jsx
+++ frontend/src/components/hires/ViewHire.jsx
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
 
-
-const ViewHire = ({setViewHire}) => {
+const ViewHire = ({setViewHire , viewHireData}) => {
 
     ViewHire.propTypes = {
         setViewHire: PropTypes.func.isRequired,
+        viewHireData: PropTypes.object.isRequired
       };
 
     const cancel = () => {
@@ -12,28 +12,29 @@ const ViewHire = ({setViewHire}) => {
       }
 
   return (
-    <div className=" absolute bg-white border-2 border-[#0E6300] w-[75%] mb-6 top-14">
+    <div className=" absolute bg-white border-2 border-[#0E6300] w-[75%] mb-6 top-11 right-11 xl:top-5">
          <div className=' xl:flex xl:flex-col justify-between mx-10 my-5'>
               <div className='mr-[20px]'>
                 <h1 className="text-2xl underline font-bold text-center mb-5">Trip Details</h1>
 
                 <div className=' xl:flex justify-between'>
                   <div>
-                    <p className=' text-lg font-semibold leading-8'>Start Date : &nbsp;&nbsp; </p>
-                    <p className=' text-lg font-semibold leading-8'>End Date : &nbsp;&nbsp;</p>
-                    <p className=' text-lg font-semibold leading-8'>Vehicle Type : &nbsp;&nbsp;</p>
-                    <p className=' text-lg font-semibold leading-8'>Vehicle Sub-Catagory : &nbsp;&nbsp; </p>
-                    <p className=' text-lg font-semibold leading-8'>Air Condition : &nbsp;&nbsp; </p>
-                    <p className=' text-lg font-semibold leading-8'>No of Passengers : &nbsp;&nbsp; </p>
+                    <p className=' text-lg font-semibold leading-8'>Start Date : &nbsp;&nbsp; {viewHireData.startDate} </p>
+                    <p className=' text-lg font-semibold leading-8'>End Date : &nbsp;&nbsp; {viewHireData.endDate}</p>
+                    <p className=' text-lg font-semibold leading-8'>Vehicle Type : &nbsp;&nbsp;{viewHireData.vehicleType}</p>
+                    <p className=' text-lg font-semibold leading-8'>Vehicle Sub-Catagory : &nbsp;&nbsp; {viewHireData.vehicleSubcategory}</p>
+                    <p className=' text-lg font-semibold leading-8'>Air Condition : &nbsp;&nbsp; {viewHireData.airCondition ? "Yes" : "No"}</p>
+                    <p className=' text-lg font-semibold leading-8'>No of Passengers : &nbsp;&nbsp; {viewHireData.passengerCount}</p>
                   </div>
 
                   <div>
-                    <p className=' text-lg font-semibold leading-8'>Assigned Vehicle : &nbsp;&nbsp;</p>
-                    <p className=' text-lg font-semibold leading-8'>Assigned Driver : &nbsp;&nbsp;</p>
-                    <p className='text-lg font-semibold leading-8'>Start Point :&nbsp;&nbsp;</p>
-                    <p className=' text-lg font-semibold leading-8'>End Point : &nbsp;&nbsp; </p>
-                    <p className=' text-lg font-semibold leading-8'>Round Trip : &nbsp;&nbsp;</p>
-                    <p className=' text-lg font-semibold leading-8'>Distence : &nbsp;&nbsp; </p>
+                    <p className=' text-lg font-semibold leading-8'>Assigned Vehicle : &nbsp;&nbsp; {viewHireData.vehicle}</p>
+                    <p className=' text-lg font-semibold leading-8'>Vehicle Model: &nbsp;&nbsp; BMW 5</p>
+                    <p className=' text-lg font-semibold leading-8'>Assigned Driver : &nbsp;&nbsp; {viewHireData.driver}</p>
+                    <p className='text-lg font-semibold leading-8'>Start Point :&nbsp;&nbsp; {viewHireData.startPoint}</p>
+                    <p className=' text-lg font-semibold leading-8'>End Point : &nbsp;&nbsp; {viewHireData.endPoint}</p>
+                    <p className=' text-lg font-semibold leading-8'>Round Trip : &nbsp;&nbsp; {viewHireData.tripType ? "yes" : "No"}</p>
+                    <p className=' text-lg font-semibold leading-8'>Distence : &nbsp;&nbsp; {viewHireData.distance}</p>
                   </div>
                 </div>
                 
@@ -45,10 +46,10 @@ const ViewHire = ({setViewHire}) => {
                 <h1 className="text-2xl underline font-bold text-center mb-5">Customer Details</h1>
 
                 
-                <p className=' text-lg font-semibold leading-8'>Customer Name : &nbsp;&nbsp;</p>
-                <p className=' text-lg font-semibold leading-8'>Customer Email : &nbsp;&nbsp; </p>
-                <p className=' text-lg font-semibold leading-8'>Customer Mobile : &nbsp;&nbsp; </p>
-                <p className=' text-lg font-semibold leading-8'>Customer NIC : &nbsp;&nbsp;</p>
+                <p className=' text-lg font-semibold leading-8'>Customer Name : &nbsp;&nbsp; {viewHireData.cusName}</p>
+                <p className=' text-lg font-semibold leading-8'>Customer Email : &nbsp;&nbsp; {viewHireData.cusEmail}</p>
+                <p className=' text-lg font-semibold leading-8'>Customer Mobile : &nbsp;&nbsp; {viewHireData.cusMobile}</p>
+                <p className=' text-lg font-semibold leading-8'>Customer NIC : &nbsp;&nbsp; {viewHireData.cusNic}</p>
 
               </div>
 
