d93f91089a1d37e2b17c6046841cb4585020f2f8
hxshan <galgodageheshan@gmail.com>
2024-04-03 12:54:11 +0530

    package resolved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,16 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import DriverDashboard from "./pages/driver/DriverDashboard";
 import VehicleDashboard from "./pages/vehicle/VehicleDashboard";
-
 import HireDashboard from "./pages/hires/HireDashboard";
 import {Login,Navbar, TopBar} from "./pages/shared"
-
 import CaseFileForm from "./components/EAM/CaseFileForm";
-import { useAuthContext } from "./hooks/useAuthContext";
 import { CreateMaintainceForm } from "./components/VR/CreateMaintainceForm";
 import AddVehicle from "./pages/vehicle/AddVehicle"
 import AddContract from "./pages/contract/AddContract";
 import { MaintainceDashboard } from "./pages/Maintains/MaintainceDashboard";
 import { EditMaintainceOrder } from "./components/VR/EditMaintainceOrder";
-import ContractDasbored from "./pages/contract/ContractDasbored";
+import ContractDasboard from "./pages/contract/ContractDasboard";
 import FinanaceDashboard from "./pages/finance/FinanaceDashboard";
 import {AdminDashboard,Roles,EditRoles} from "./pages/admin";
 
@@ -47,7 +43,7 @@ function App() {
           <Route path="/driver" element={<DriverDashboard />} />
           <Route path="/Mdashboard" element={<MaintainceDashboard />} />
           <Route path="/vehiclemaintain/edit/:id" element={<EditMaintainceOrder/>}/>
-          <Route path="/Contract/Dashbored" element={<ContractDasbored/>}/>
+          <Route path="/Contract/Dashbored" element={<ContractDasboard/>}/>
           <Route path = "/finance" element ={<FinanaceDashboard />} />
         </Routes>
         </div>
