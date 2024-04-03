4eed102ad3fcf3dadd0aedb7de9d8e04334599e8
hxshan <galgodageheshan@gmail.com>
2024-04-03 21:21:30 +0530

    middleware fix

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const {login,refresh,logout}=require("../controllers/authController")
-const verifyJWT = require("../middleware/verifyJWT")
 
 const router = express.Router()
 
