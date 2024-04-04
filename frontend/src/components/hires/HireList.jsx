6afa53de6388518ff13a49cb72f34c1b67b5564c
Chamod Perera <chamod914@gmail.com>
2024-04-04 22:50:44 +0530

    Update Hire List

--- frontend/src/components/hires/HireList.jsx
+++ frontend/src/components/hires/HireList.jsx
@@ -1,7 +1,9 @@
-import HireTable from "./HireTable"
 import PropTypes from 'prop-types';
 import SearchHire from "./SearchHire";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import ViewHire from "./ViewHire";
+import axios from "@/api/axios";
+import useAxios from "@/hooks/useAxios";
 
 
 const HireList = ({showForm ,setShowForm}) => {
@@ -24,6 +26,84 @@ const HireList = ({showForm ,setShowForm}) => {
         setSearchType(searchT)
     }
 
+    const [hireData, error, loading, axiosFetch] = useAxios();
+
+    useEffect(() => {
+        const fetchHires = async () => {
+
+            axiosFetch({
+                axiosInstance: axios,
+                method: "GET",
+                url: "/hire",
+              });
+              
+            /*try {
+                const response = await axios.get('http://localhost:3000/api/hire');
+                setHireData(response.data);
+            } catch (error) {
+                console.error('Error fetching data in Hire: ' + error);
+            }*/
+        };
+
+        fetchHires();
+    }, []);
+
+    const [viewHire, setViewHire] = useState(false);
+    const [viewHireData, setViewHireData] = useState(null);
+    const [reload, setReload] = useState(0);
+
+    const handleView = (hId) => {
+        const selected = hireData.find((hire) => hire._id === hId);
+        setViewHireData(selected);
+        setViewHire(true);
+    };
+
+    // Search function
+    useEffect(() => {
+        const result = hireData.filter((hire) => {
+            if (searchType === 'vehicleNumber') {
+                return hire.vehicle.toLowerCase().includes(vehicleNo.toLowerCase());
+            } else if (searchType === 'customerMobile') {
+                return hire.cusMobile.includes(vehicleNo);
+            }
+            return false;
+        });
+        setResults(result);
+    }, [hireData, vehicleNo, searchType, setResults]);
+    
+
+    // Handle Delete
+    /*
+    const deleteHire = async (id) => {
+        try {
+            await axios.delete(`http://localhost:3000/api/hire/${id}`);
+            const updatedHireData = hireData.filter((hire) => hire._id !== id);
+            setHireData(updatedHireData);
+            setResults(updatedHireData); // Update the results with the new data
+            console.log('Hire deleted successfully');
+        } catch (error) {
+            console.error('Error deleting hire: ' + error);
+        }
+    };*/
+
+    const deleteHire =async(id) => {
+        if(confirm("Are you sure you want to Delete the following")){
+          await axiosFetch({
+            axiosInstance: axios,
+            method: "DELETE",
+            url: `/hire/${id}`,
+          });
+          if(!error){
+            setReload(reload + 1);
+          } 
+        }
+      };
+    
+    
+      if (loading) {
+        return <p>Loading...</p>;
+      }
+
   return (
     <div className="w-full h-full flex bg-gray-200 px-[20px] py-[20px] justify-center items-center">
             <div className="w-full h-full bg-white">
@@ -37,13 +117,56 @@ const HireList = ({showForm ,setShowForm}) => {
                     <div className="pt-[10px]">
                         <button className="px-4 py-2 text-white bg-black rounded-md hover:bg-gray-800 focus:outline-none" onClick={()=>{setShowForm(!showForm)}}>
                             Add
-                    </button>
+                        </button>
+                    </div>
+
                 </div>
 
+                <div>
+                    <div className="w-full h-full flex px-2 py-[20px] justify-center align-center xl:px-[60px] xl:py-[50px] ">
+                        <table className="w-full text-center ">
+                            <thead className="border-b-2 border-black">
+                                <tr>
+                                    <th className="px-4 py-2">Vehicle No</th>
+                                    <th className="px-4 py-2">Start Date</th>
+                                    <th className="px-4 py-2">End Date</th>
+                                    <th className="px-4 py-2">Cus Mobile</th>
+                                    <th className="px-4 py-2">Status</th>
+                                    <th className="px-4 py-2">Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {(vehicleNo ? results : hireData).map((hire) =>
+                                    <tr key={hire._id} className="border-b-2 border-black">
+                                        <td className="px-4 py-2">{hire.vehicle}</td>
+                                        <td className="px-4 py-2">{new Date(hire.startDate).toLocaleDateString()}</td>
+                                        <td className="px-4 py-2">{new Date(hire.endDate).toLocaleDateString()}</td>
+                                        <td className="px-4 py-2">{hire.cusMobile}</td>
+                                        <td className="px-4 py-2">{hire.hireStatus}</td>
+                                        <td className="px-4 py-4 flex justify-between items-baseline">
+                                            <button
+                                                className="px-2 py-1 bg-[#D4D800] text-white rounded-md mr-2"
+                                                onClick={() => handleView(hire._id)}>
+                                                View
+                                            </button>
+                                            <button
+                                                className="hidden xl:grid px-2 py-1 bg-[#A90000] text-white rounded-md"
+                                                onClick={() => deleteHire(hire._id)}>
+                                                Delete
+                                            </button>
+                                        </td>
+                                    </tr>
+                                )}
+                            </tbody>
+                        </table>
+
+                        {viewHire && <ViewHire setViewHire={setViewHire} viewHireData={viewHireData} />}
+
+                    </div>
                 </div>
 
  
-                <HireTable  vehicleNo = {vehicleNo} searchType = {searchType} setResults = {setResults} results ={results}/>
+                
             </div>
         </div>
     
