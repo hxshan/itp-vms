f061688f3b510684e28c1a6a29f326f347751d37
ishini21 <ishinishavindhya@gmail.com>
2025-10-19 00:01:11 +0530

    updated

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,10 +1,23 @@
 import axios from "axios";
-const BASE_URL='http://localhost:3000/api'
 
-export default axios.create({
-    baseURL:BASE_URL,
-    withCredentials:true
-})
+const BASE_URL = 'http://localhost:3000/api';
 
-//http://localhost:3000/api
-//once backend is hosted baseURL:'http://165.22.213.22/api'
+// Create axios instance with credentials
+const axiosInstance = axios.create({
+    baseURL: BASE_URL,
+    withCredentials: true
+});
+
+
+axiosInstance.interceptors.request.use((config) => {
+    if (config.method !== 'get') {
+        // Get CSRF token from cookie or meta tag
+        const csrfToken = csrfToken(); 
+        if (csrfToken) {
+            config.headers['X-CSRF-Token'] = csrfToken;
+        }
+    }
+    return config;
+});
+
+export default axiosInstance;
